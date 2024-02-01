9ac7a38cd5a0bceb1a7e0ad7f48d90cd937d72e0
kalin0310 <114457914+kalin0310@users.noreply.github.com>
2024-02-01 16:25:25 +0800

    Update app.py

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ openai.api_key = os.environ["OPENAI_API_KEY"]
 # GPT-3.5 Turbo 模型名稱
 model_name = "ft:gpt-3.5-turbo-1106:personal::8nM2oEXy"
 
-def GPT_response(user_input, tokens=500, temperature=1.5):
+def GPT_response(user_input, tokens=500, temperature=0.8):
     # 使用ChatCompletion進行OpenAI API呼叫，並使用傳入的參數
     chat = ChatCompletion.create(
         model=model_name,
