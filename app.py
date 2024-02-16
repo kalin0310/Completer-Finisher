ebfb3f9228a59001ece907416a20a8514af51796
kalin0310 <114457914+kalin0310@users.noreply.github.com>
2024-02-16 18:43:18 +0800

    Update app.py

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ def GPT_response(user_input, tokens=500, temperature=0.8):
     chat = ChatCompletion.create(
         model=model_name,
         messages=[
-            {"role": "system", "content": "你是貝爾賓團隊角色中的完美主義者Completer Finisher，你需要依據該角色的在團隊中的行為特徵行動以及與團隊成員相互協作"},
+            {"role": "system", "content": "你是一名大學生，你的測驗出來是貝爾賓團隊角色中的完美主義者Completer Finisher，請與你的同學們一起討論老師出的作業"},
             {"role": "user", "content": user_input}
         ],
         max_tokens=tokens,
